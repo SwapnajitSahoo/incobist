b0beaaaa7ae8f39a3b66ff42bc259ebdf4e14ae1
Swapnajit <swapnajit@mydevteam.com>
2026-03-18 07:42:41 +0530

    merge fixed

--- routes/admin.php
+++ routes/admin.php
@@ -25,20 +25,20 @@ Route::get('/dashboard', function () {
 Route::get('/navigation-setup', [NavbarMenuController::class, 'navSetup'])->name('nav_setup');
 Route::post('/navigation-setup', [NavbarMenuController::class, 'navStore'])->name('navbar-menu-store');
 
- // Page Contents
-Route::get('page-contents',                  [PageContentController::class, 'index'])  ->name('page-contents.index');
-Route::get('page-contents/create',           [PageContentController::class, 'create']) ->name('page-contents.create');
-Route::post('page-contents/store',           [PageContentController::class, 'store'])  ->name('page-contents.store');
-Route::get('page-contents/{id}/edit',        [PageContentController::class, 'edit'])   ->name('page-contents.edit');
-Route::post('page-contents/{id}/update',     [PageContentController::class, 'update']) ->name('page-contents.update');
-Route::post('page-contents/{id}/delete',     [PageContentController::class, 'destroy'])->name('page-contents.delete');
+// Page Contents
+Route::get('page-contents', [PageContentController::class, 'index'])->name('page-contents.index');
+Route::get('page-contents/create', [PageContentController::class, 'create'])->name('page-contents.create');
+Route::post('page-contents/store', [PageContentController::class, 'store'])->name('page-contents.store');
+Route::get('page-contents/{id}/edit', [PageContentController::class, 'edit'])->name('page-contents.edit');
+Route::post('page-contents/{id}/update', [PageContentController::class, 'update'])->name('page-contents.update');
+Route::post('page-contents/{id}/delete', [PageContentController::class, 'destroy'])->name('page-contents.delete');
 
 // Page Sections
-Route::post('page-sections/store/{pageId}',  [PageSectionController::class, 'store'])  ->name('page-sections.store');
-Route::post('page-sections/{id}/update',     [PageSectionController::class, 'update']) ->name('page-sections.update');
-Route::post('page-sections/{id}/delete',     [PageSectionController::class, 'destroy'])->name('page-sections.delete');
-Route::post('page-sections/reorder',         [PageSectionController::class, 'reorder'])->name('page-sections.reorder');
+Route::post('page-sections/store/{pageId}', [PageSectionController::class, 'store'])->name('page-sections.store');
+Route::post('page-sections/{id}/update', [PageSectionController::class, 'update'])->name('page-sections.update');
+Route::post('page-sections/{id}/delete', [PageSectionController::class, 'destroy'])->name('page-sections.delete');
+Route::post('page-sections/reorder', [PageSectionController::class, 'reorder'])->name('page-sections.reorder');
 
 // SEO Meta
-Route::post('seo-meta/store/{pageId}',       [SeoMetaController::class, 'store'])      ->name('seo-meta.store');
-Route::post('seo-meta/{id}/update',          [SeoMetaController::class, 'update'])     ->name('seo-meta.update');
+Route::post('seo-meta/store/{pageId}', [SeoMetaController::class, 'store'])->name('seo-meta.store');
+Route::post('seo-meta/{id}/update', [SeoMetaController::class, 'update'])->name('seo-meta.update');
